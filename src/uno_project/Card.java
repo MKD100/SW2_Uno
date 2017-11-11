e11283a0615014f5dd4ae2765c6a2a23e95fc3d3
matth <matth@DESKTOP-4QM2RFT>
2017-11-10 23:25:58 -0600

    Actual changes to the Card class, be warned friends. I'm inebriated

--- src/uno_project/Card.java
+++ src/uno_project/Card.java
@@ -10,11 +10,57 @@ package uno_project;
  * @author matth
  */
 public class Card {
-    int [] hand;
-    String color; 
+    /*
+        color key 
+        red=0
+        green=1
+        blue=2
+        yellow=3
+        special=4
+    */
+    String color;
+    /*
+        Skip
+        Reverse
+        Wild
+        Wild+
+    */
+    String num;
     
     public void setColor(int i){
-        if(i == 0)
+        if(i == 0){
+            this.color="red";
+        }
+        if(i == 1){
+            this.color="green";
+        }
+        if(i == 2){
+            this.color="blue";
+        }        
+        if(i == 3){
             this.color="Yellow";
+        }        
     }
+    public void setNumber(int i){
+        if(i > 0 || i <11){
+            this.num = Integer.toString(i);
+        }
+        //SKIP
+        if(i==11){
+            this.num = "Skip";
+        }
+        //REVERSE
+        if(i==12){
+            this.num = "Reverse";
+        }
+        //WILD
+        if(i==13){
+            this.num = "Wild";
+        }       
+        //WILD +4
+        if(i==14){
+            this.num = "Wild+4";
+        }     
+    }
+    
 }
