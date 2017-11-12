c7e5cf2725d99b16149ff30f682e076ec1a923b1
Alonzo Dominguez <Alonzo Dominguez@DESKTOP-P6TH9PG>
2017-11-12 13:32:23 -0600

    First Commit, Working on the Card and Deck classes.

--- src/uno_project/Card.java
+++ src/uno_project/Card.java
@@ -9,12 +9,52 @@ package uno_project;
  *
  * @author matth
  */
-public class Card {
-    int [] hand;
-    String color; 
-    
-    public void setColor(int i){
-        if(i == 0)
-            this.color="Yellow";
+public class Card implements Comparable<Card>{
+    private int color, value;
+    private String[] cardColor = {"Red", "Yellow", "Blue", "Green"};
+    private String[] cardValue = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven",
+                                   "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen"};
+                                    /*
+                                    10=Skip , 11=Reverse , 12=DrawTwo , 
+                                    13=WildChangeColor , 14=WildChangeColorDrawFour
+                                    */
+    public Card(){
     }
+    public Card(int cColor, int cValue){
+        color = cColor;
+        value = cValue;
+    }
+    public String toString(){
+        String cardInfo = cardValue[value]+cardColor[color];
+        return cardInfo;
+    }
+    public int getSuit(){
+        return this.color;
+    }
+    public int getValue(){
+        return this.value;
+    }
+    public void setSuit(int suitx){
+            this.color = suitx;
+    }
+    public void setValue(int valuex){
+        this.value = valuex;
+    }
+    public String getImage(){
+        return ("/images"+"/"+Integer.toString(this.color)+".png");
+    }
+    @Override
+    public int compareTo(Card compareCard) {
+        
+        int compareSuit = ((Card)compareCard).getSuit();
+        int compareValue=((Card)compareCard).getValue();
+        if(this.color == compareSuit)
+        {
+            /* For Ascending order of values */
+            return this.value-compareValue;
+         }
+        /* For Ascending order of suits */
+        else return this.color - compareSuit; 
+     }
+
 }
--- src/uno_project/Deck.java
+++ src/uno_project/Deck.java
@@ -4,18 +4,58 @@
  * and open the template in the editor.
  */
 package uno_project;
-
 /**
  *
  * @author matth
  */
+import java.util.*;
+
 public class Deck {
-  int num =0;
- 
-  
-  
-  
-  
-  
-  
+    ArrayList <Card> a = new ArrayList<Card>();
+    private Card card = new Card();
+    /*public String printAllCard(){
+        for (int i=0; i<4; i++)
+            
+    }*/
+    public Deck(){
+        this.makeDeck();
+    }
+    public void makeDeck(){
+        String b;
+        for (int i=0; i<4; i++){
+            Card c1 = new Card();
+            
+        }
+        for (int i=0; i<4; i++){
+            for (int j=0; j<13; j++){
+                Card c1 = new Card();
+                c1.setSuit(i);
+                c1.setValue(j);
+                a.add(c1);
+                
+            }
+        }
+    }
+    public void shuffle(){
+        Collections.shuffle(this.a);
+    }
+    
+    public Card getCard(){
+        Card c1 = new Card();
+        c1 = a.get(0);
+        a.remove(0);
+        shuffle();
+        return c1;
+    }  
+    public void printAllCards(){
+        for(int i=0; i<a.size(); i++){
+            Card a = new Card();
+            a = this.a.get(i);
+            System.out.println(a.toString());
+        }
+    }
+    public void sort(){
+        Collections.sort(this.a);
+    }
 }
+
